a990c044951b4205d13b490b0d21647c3e726619
mojtabamonfared <mojmon42@gmail.com>
2016-12-26 22:22:17 +0330

    last commit to unprivating

--- script.py
+++ script.py
@@ -3,40 +3,58 @@ import sys
 import os
 import re
 import io
-import compiler
+# import compiler
 from time import sleep
 import PythonColorize as _
-
-init_code = """#!/usr/bin/python
-
 import Tkinter
+from Tkinter import *
+
+print _.colorize(text="""  ,ad8888ba,                                          
+ d8"'    `"8b                                         
+d8'                                                   
+88             88       88  8b,dPPYba,   8b       d8  
+88      88888  88       88  88P'    "8a  `8b     d8'  
+Y8,        88  88       88  88       d8   `8b   d8'   
+ Y8a.    .a88  "8a,   ,a88  88b,   ,a8"    `8b,d8'    
+  `"Y88888P"    `"YbbdP'Y8  88`YbbdP"'       Y88'     
+                            88               d8'      
+                            88              d8'       """, color='cyan', style='bold')
+			print _.colorize(text='\r[About] an gui Generator written in python', color='green')
+
+
+# Syntax Doc:
+"""
+ *
+ * 
+ * button(arg*)
+ * label(arg*)
+ * alert(Text)
+ *
+ * 
+"""
+
+
+def compiler(path):
+	if re.findall(r'^~/+\.gupy', path):
+		with open(path, 'r').read() as f:
+			EXEC_compile = """import Tkinter
 top = Tkinter.Tk()
+{f}
+top.mainloop()""".format(f=f)
+			exec(EXEC_compile)
 
-#####################
-# Your Code Should be here :)
-#####################
-
-top.mainloop()"""
 
 def init(path):
 	if re.findall(r'^~/+', path):
 		print _.colorize(text='[*] init project Started...', color='yellow')
 		print _.colorize(text='[*] change current dir to {}'.format(path), color='yellow')
 		os.system('touch {path}/gui.gupy && subl {path}/gui.gupy'.format(path=path))
-		this_script_dir = os.path.dirname(__file__)
-		rel_path = path + '/gui.gupy'
-		path_for_new_file = os.path.join(this_script_dir, rel_path)
-		with io.FileIO(path_for_new_file, "w") as file:
-			file.write(init_code)
-			sleep(3)
-			print _.colorize(text='[+] Your Request Succefully Finished!', color='cyan')
+		print _.colorize(text='[+] Your Request Succefully Finished!', color='cyan')
 	else:
 		print _.colorize(text='[!] Please start your path with ~', color='red')
 
 
 def main():
-	# print cmd
-	# arg = cmd.split(' ')[1]
 	while True:
 		try:
 			cmd = raw_input(_.colorize(text="> ", color='red'))
@@ -50,7 +68,7 @@ def main():
 					print "\nBye ;)"
 					break
 			if cmd == str("compile"):
-				AskForcompile = raw_input(_.colorize(text="Path (start with ~ and End with 'name.gupy'): ", color='green', style='bold'))
+				AskForcompile = str(raw_input(_.colorize(text="Path (start with ~ and End with 'name.gupy'): ", color='green', style='bold')))
 				compiler(path=AskForcompile)
 		except KeyboardInterrupt:
 			print _.colorize(text='\n[*] Stopping Script...', color='red')
